dfad3f4672e2d3819767a8a440e33adf8a69091a
Stilllee <orodae@gmail.com>
2023-12-26 22:13:47 +0900

    기본 UI

--- App.js
+++ App.js
@@ -1,26 +1,51 @@
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "tomato",
-        }}
-      ></View>
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: "teal",
-        }}
-      ></View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "orange",
-        }}
-      ></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f6d38c",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: 600,
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    backgroundColor: "teal",
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  desc: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+});
+
+export default App;
